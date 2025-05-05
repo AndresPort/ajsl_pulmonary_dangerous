9ccb8a4387c5d8a6839dab8aba4108b9faf516d4
lozan <lozanosamuel0424@gmail.com>
2025-05-05 00:23:54 -0500

    Se implemento que el header fuera fijo y las rutas de las enfermedades

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,10 +1,10 @@
-import { NavLink } from "react-router";
+import { useNavigate } from "react-router";
 import "./Header.css";
 import logo from "../../assets/logo.png";
-import { useNavigate } from "react-router";
 
 const Header = () => {
   const navigate = useNavigate();
+
   return (
     <header className="header">
       <div className="logo-container">
@@ -13,18 +13,29 @@ const Header = () => {
         </a>
         <h1 className="tittle_name">AJLS Pulmonary Dangers</h1>
       </div>
-      <div className="nav-buttons">
-        <nav className="nav-links">
-          <a onClick={() => navigate("/")}>Inicio</a>
-          <a href="#">Enfermedades</a>
-          <a href="#">Quiz interactivo</a>
-          <a href="#">Sobre nosotros</a>
-        </nav>
+      <nav className="nav-bar">
+        <ul className="nav-links">
+          <li><a onClick={() => navigate("/")}>Inicio</a></li>
+          
+          {/* Dropdown */}
+          <li className="dropdown">
+            <a>Enfermedades</a>
+            <ul className="dropdown-menu">
+              <li><a onClick={() => navigate("/lungs-cancer")}>Cáncer de pulmón</a></li>
+              <li><a onClick={() => navigate("/e-c-p")}>Enfermedad cronica pulmonar</a></li>
+              <li><a onClick={() => navigate("/pneumonia")}>Neumonía</a></li>
+              <li><a onClick={() => navigate("/tuberculosis")}>Tuberculosis</a></li>
+            </ul>
+          </li>
+
+          <li><a href="#">Quiz interactivo</a></li>
+          <li><a href="#">Sobre nosotros</a></li>
+        </ul>
         <div className="buttons">
           <button className="btn white">Ingresar</button>
           <button className="btn green">Registrarse</button>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
